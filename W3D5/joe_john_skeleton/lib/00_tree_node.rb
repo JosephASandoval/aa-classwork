cd3b94b35c2417f2bc7cb48b0cebde8ca17a0d89
KingApe714 <jonathan.diaz714@gmail.com>
2020-12-04 17:49:31 -0500

    updated w3d5 tree_node

--- W3D5/joe_john_skeleton/lib/00_tree_node.rb
+++ W3D5/joe_john_skeleton/lib/00_tree_node.rb
@@ -9,24 +9,35 @@ class PolyTreeNode
   end
 
   def parent=(node)
+    if parent
+      parent.children.delete(self)
+    end
     @parent = node
+    node.children << self if self.parent != nil
+    return if self.parent == node
   end
 
+  def add_child(child_node)
+    child_node.parent=(self)
+  end
+
+  def remove_child(child_node)
+    raise "Error" if !self.children.include?(child_node)
+    child_node.parent=(nil)
+  end
 
 end
 
 
 
 
-f = PolyTreeNode.new("f")
-g = PolyTreeNode.new("e")
-d = PolyTreeNode.new("d")
-e = PolyTreeNode.new("e")
-b = PolyTreeNode.new("b", [d, e])
-c = PolyTreeNode.new("c", [f, g])
-a = PolyTreeNode.new("a", [b, c])
-
-p a
+@f = PolyTreeNode.new("f")
+@g = PolyTreeNode.new("e")
+@d = PolyTreeNode.new("d")
+@e = PolyTreeNode.new("e")
+@b = PolyTreeNode.new("b", [@d, @e])
+@c = PolyTreeNode.new("c", [@f, @g])
+@a = PolyTreeNode.new("a", [@b, @c])
 
 
 # tree = [a, [b, [d, e], c, [f, g]]]
