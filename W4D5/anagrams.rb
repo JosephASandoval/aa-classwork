7c4f5d42b7a21a75870818ba1f2a3d5e645b28d5
JosephASandoval <josephasandoval17@gmail.com>
2020-12-11 14:38:16 -0800

    Finished anagrams

--- W4D5/anagrams.rb
+++ W4D5/anagrams.rb
@@ -12,9 +12,9 @@ end
 # p first_anagram?("gizmoasds", "sallyasdf")    #=> false
 # p first_anagram?("elvis", "lives")    #=> true
 
-#O(n^2)
+#O(n * m)
 def second_anagram?(first_str, second_str)
-    first_str.each_char.with_index do |ele, i|
+    first_str.each_char.with_index do |ele, i| # n * m
         idx = second_str.index(ele)
         second_str[idx] = "" unless idx.nil?
     end
@@ -25,24 +25,28 @@ end
 # p second_anagram?("gizmo", "sally")    #=> false
 # p second_anagram?("elvis", "lives")    #=> true
 
-#O(nlogn)
+#O(nlogn + mlogm)
 def third_anagram?(first_str, second_str)
-  first_sorted = first_str.chars.sort
-  second_sorted = second_str.chars.sort
+  first_sorted = first_str.chars.sort # n + nlogn
+  second_sorted = second_str.chars.sort # m + mlogm
   first_sorted == second_sorted
 end
 
 # p third_anagram?("gizmo", "sally")    #=> false
 # p third_anagram?("elvis", "lives")    #=> true
 
-# O(n)
+# O(n + m)
 def fourth_anagram?(first_str, second_str)
-  # first_count = Hash.new(0)
-  # second_count = Hash.new(0)
-  # first_str.each_char { |char| first_count[char] += 1 }
-  # second_str.each_char { |char| second_count[char] += 1 }
-  # first_count == second_count
+  first_count = Hash.new(0)
+  second_count = Hash.new(0)
+  first_str.each_char { |char| first_count[char] += 1 } # n
+  second_str.each_char { |char| second_count[char] += 1 } # m
+  first_count == second_count
 
+
+
+
+# O(n)
   anagram = true
   count = Hash.new(0)
   first_str.each_char { |char| count["first_#{char}"] += 1 }
@@ -53,5 +57,5 @@ def fourth_anagram?(first_str, second_str)
   anagram
 end
 
-p fourth_anagram?("gizmo", "sally")    #=> false
-p fourth_anagram?("elvis", "lives")    #=> true
+# p fourth_anagram?("gizmo", "sally")    #=> false
+# p fourth_anagram?("elvis", "lives")    #=> true
