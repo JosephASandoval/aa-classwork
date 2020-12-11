ff5fd1ee48fa823d7b7ed1d0c5990a5d42cc84b1
caroger <rogerhu1989@gmail.com>
2020-12-10 16:00:22 -0800

    rspec for valid_move?

--- W4D4/hanoi/lib/hanoi.rb
+++ W4D4/hanoi/lib/hanoi.rb
@@ -6,6 +6,13 @@ class Hanoi
     @piles = [[3, 2, 1],[],[]]
   end
 
+[[3], [2],[1]]
+
+[[],[2],[1,3]] -> invalid
+  def move(start_pile, end_pile)
+    disk = @piles[start_pile].pop
+    @piles[end_pile] << disk
+  end
 end
 
 
--- W4D4/hanoi/spec/hanoi_spec.rb
+++ W4D4/hanoi/spec/hanoi_spec.rb
@@ -2,13 +2,40 @@ require "rspec"
 require "hanoi"
 
 describe Hanoi do
+
   subject(:hanoi) { Hanoi.new }
+
   describe "#initialize" do
     it "creates starting piles with 3 disks on the left most pile" do
       expect(hanoi.piles).to eq([[3, 2, 1],[],[]])
     end
   end
 
-  describe
+  describe "#move(start_pile, end_pile)" do
+    it "moves the top disk on start_pile to the top of end_pile" do
+      hanoi.move(0,2)
+      expect(hanoi.piles).to eq([[3,2],[],[1]])
+    end
+  end
+
+  describe "#valid_move?(start, end)" do
+    it "checks the validity of the move" do
+      before(:each) do
+        hanoi.move(0,2)
+      end
+      #@piles[start] is empty => invalid
+      #disk = @piles[start].last
+      #disk < @piles[end].last => valid
+
+      expect(hanoi.valid_move?(0, 2)).to be false
+      expect(hanoi.valid_move?(1, 2)).to be false
+      expect(hanoi.valid_move?(1, 0)).to be false
+      expect(hanoi.valid_move?(0, 1)).to be true
+      expect(hanoi.valid_move?(2, 1)).to be true
+      expect(hanoi.valid_move?(2, 0)).to be true
+            
+    end
+  end
+
 
 end
