e474a0a422bac2607b605afc07246341652e0a3c
IsoVoyd <dubnersamuel@gmail.com>
2020-12-11 16:09:59 -0800

    finish #two_sum

--- W4D5/two_sum_problem.rb
+++ W4D5/two_sum_problem.rb
@@ -16,12 +16,13 @@ end
 # p bad_two_sum?(arr, 6) # => should be true
 # p bad_two_sum?(arr, 10) # => should be false
 
-# O(n^2) => sorting can make it more efficient
+# O(nlogn) => sorting can make it more efficient
 def okay_two_sum?(arr, target)
-  arr = arr.sort
-  arr.each.with_index do |ele, idx|
-      target_index = arr.rindex(target - ele)
-      return true if !target_index.nil? && idx != target_index
+    arr = arr.sort
+    arr.each.with_index do |ele, idx|
+        # target_index = arr.rindex(target - ele)
+        target_index = arr.bsearch_index { |x| x == target - ele }
+        return true if !target_index.nil? && idx != target_index
     end
     false
 end
@@ -29,3 +30,21 @@ end
 arr = [0, 1, 5, 7]
 p okay_two_sum?(arr, 6) # => should be true
 p okay_two_sum?(arr, 10) # => should be false
+
+#O(n)
+def two_sum?(arr, target)
+    hash = Hash.new(0)
+    arr.each_with_index do |ele, i|
+        hash[ele] += 1
+        if (target - ele) == ele
+            return true if hash[target - ele] == 2
+        else
+            return true if hash[target - ele] == 1
+        end
+    end
+    false
+end
+
+# arr = [0, 1, 5, 7]
+# p two_sum?(arr, 6) # => should be true
+# p two_sum?(arr, 10) # => should be false
