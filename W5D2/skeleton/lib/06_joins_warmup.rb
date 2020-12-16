20c06bf51226c009e660ca1d86564bbb7cf8d55a
chris <chrispikacho@gmail.com>
2020-12-15 16:36:13 -0800

    Started 07

--- W5D2/skeleton/lib/06_joins_warmup.rb
+++ W5D2/skeleton/lib/06_joins_warmup.rb
@@ -127,6 +127,15 @@ end
 def alien_cast
   # Obtain the cast list for the film 'Alien'
   execute(<<-SQL)
-    
+    SELECT
+      name
+    FROM
+      actors
+    JOIN
+      castings ON actors.id = castings.actor_id
+    JOIN
+      movies ON castings.movie_id = movies.id
+    WHERE
+      title = 'Alien';
   SQL
 end
