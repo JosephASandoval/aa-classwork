0fbdb007f95e783402dad391580a9808b21ee868
James Koenig <Koenig.James@Live.com>
2020-12-16 15:07:52 -0800

    working through database setup file, switching roles

--- W5D3/import_db.sql
+++ W5D3/import_db.sql
@@ -8,7 +8,32 @@ CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title TEXT,
   body TEXT,
-  author_id INTEGER,
+  author_id INTEGER NOT NULL,
   FOREIGN KEY(author_id) REFERENCES users(id)
 );
 
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES question(id)
+);
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  parent_id INTEGER,
+  user_id INTEGER NOT NULL,
+  body_id INTEGER NOT NULL,
+
+  FOREIGN KEY (parent_id) REFERENCES replies(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES question(id)
+)
+
+CREATE TABLE question_likes (
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+)
