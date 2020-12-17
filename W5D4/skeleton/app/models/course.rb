b0357e6fbf3affcaa8851fac83736bfa81a7e77a
Michelle Ha <michelle1992us@yahoo.com>
2020-12-17 14:35:38 -0800

    finished enrollment

--- W5D4/skeleton/app/models/course.rb
+++ W5D4/skeleton/app/models/course.rb
@@ -1,2 +1,8 @@
 class Course < ApplicationRecord
+    has_many :enrollment,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+    
+
 end
--- W5D4/skeleton/app/models/user.rb
+++ W5D4/skeleton/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :enrollment,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+    
+
 end
