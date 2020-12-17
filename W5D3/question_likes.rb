e301058984b253281d2b9f47ee7b00121d35cd69
JosephASandoval <josephasandoval17@gmail.com>
2020-12-16 16:59:53 -0800

    Updated W5D3

---
+++ W5D3/question_likes.rb
@@ -0,0 +1,13 @@
+require_relative "questions_database"
+
+class QuestionLikes
+  
+  def initialize
+    
+  end
+
+  def self.find_by_id
+
+  end
+
+end
---
+++ W5D3/users.rb
@@ -0,0 +1,13 @@
+require_relative "questions_database"
+
+class Users
+  
+  def initialize
+    
+  end
+
+  def self.find_by_id
+
+  end
+
+end
