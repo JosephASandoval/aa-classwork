6e8f803ae720bbf4f9f412c2f48ca6dfe41edd44
JosephASandoval <josephasandoval17@gmail.com>
2020-12-31 16:39:52 -0800

    Start phase 2

--- W6D5/cats/app/models/cat.rb
+++ W6D5/cats/app/models/cat.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'action_view'
 include ActionView::Helpers::DateHelper
 
@@ -8,5 +21,11 @@ class Cat < ApplicationRecord
   def age
     time_ago_in_words(self.birth_date)
   end
+
+  has_many :cat_rental_requests,
+    primary_key: :id, 
+    foreign_key: :cat_id,
+    class_name: :CatRentalRequest,
+    dependent: :destroy
 
 end
