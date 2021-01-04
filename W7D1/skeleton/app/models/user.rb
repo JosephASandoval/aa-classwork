556ec2867aaf9850f746d3c2015b9a884b4129c9
JosephASandoval <josephasandoval17@gmail.com>
2021-01-04 15:15:49 -0800

    Started the users controller

--- W7D1/skeleton/app/models/user.rb
+++ W7D1/skeleton/app/models/user.rb
@@ -14,11 +14,21 @@ class User < ApplicationRecord
     validates :password_digest, presence: true
     validates :session_token, presence: true, uniqueness: true
     validates :password, length: {minimum: 6}, allow_nil: true
-
+    
     after_initialize :ensure_session_token
-
-    #in order to the password, need to read the password
+    
+    #in order to validate the password, we need to read the password
     attr_reader :password
+
+    def self.find_by_credentials(username, pw)
+        user = User.find_by(user_name: username)
+    
+        if user && user.is_password?(pw)
+            user 
+        else 
+            nil
+        end
+    end
     
     def reset_session_token!
         self.session_token = SecureRandom::urlsafe_base64
@@ -34,4 +44,9 @@ class User < ApplicationRecord
         self.password_digest = BCrypt::Password.create(password)
         @password = password
     end
+    
+    def is_password?(pw) 
+        BCrypt::Password.new(self.password_digest).is_password?(pw)
+    end
+    
 end
