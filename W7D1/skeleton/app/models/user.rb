6b79970be4b263b3e82bbecbbcad09b0d74e5d5c
annieyxu <annieyxu@berkeley.edu>
2021-01-04 14:41:19 -0800

    setter

--- W7D1/skeleton/app/models/user.rb
+++ W7D1/skeleton/app/models/user.rb
@@ -1,2 +1,37 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    validates :user_name, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    after_initialize :ensure_session_token
+
+    #in order to the password, need to read the password
+    attr_reader :password
+    
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
 end
