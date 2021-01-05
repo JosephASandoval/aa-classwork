7815679dff732155b56c489809a17b8c25e7947a
annieyxu <annieyxu@berkeley.edu>
2021-01-04 16:28:36 -0800

    phase4

--- W7D1/skeleton/app/controllers/users_controller.rb
+++ W7D1/skeleton/app/controllers/users_controller.rb
@@ -5,7 +5,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+
+  end
+
+  private 
+  def user_params
+    self.params.require(:user).permit(:user_name, :password)
   end
 
 
