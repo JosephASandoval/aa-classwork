964d04a976425876c4c228a03571c9230b760b95
JosephASandoval <josephasandoval17@gmail.com>
2021-01-06 21:14:27 -0800

    Added W7D3

--- W7D1/skeleton/app/controllers/sessions_controller.rb
+++ W7D1/skeleton/app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 
     if @user
         login(@user)
-        redirect_to user_url(@user)
+        redirect_to cat
     else
         render :new
     end
