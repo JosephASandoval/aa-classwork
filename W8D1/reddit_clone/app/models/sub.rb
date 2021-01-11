de3230ea5a73e15492e3a5b3771b27471b4fadcd
JosephASandoval <josephasandoval17@gmail.com>
2021-01-11 10:24:07 -0800

    Updated

---
+++ W8D1/reddit_clone/app/models/sub.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id           :bigint           not null, primary key
+#  title        :string           not null
+#  description  :text             not null
+#  moderator_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Sub < ApplicationRecord
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+
+
+end
