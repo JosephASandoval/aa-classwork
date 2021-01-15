96423bd2e4f6aefd8095fb9b7d852f50bd04825c
timjkim210 <timjkim210@gmail.com>
2021-01-15 15:16:36 -0800

    hi

--- W8D5/intro_to_callbacks_exercises.js
+++ W8D5/intro_to_callbacks_exercises.js
@@ -5,10 +5,10 @@ class Clock {
         this.minutes = timeNow.getMinutes();
         this.seconds = timeNow.getSeconds();
 
-        let time = printTime();
-        this.time;
+        this.printTime();
+        
 
-        setInterval(this._tick, 1000);
+        setInterval(this._tick.bind(this), 1000);
         // 1. Create a Date object.
         // 2. Store the hours, minutes, and seconds.
         // 3. Call printTime.
@@ -23,7 +23,17 @@ class Clock {
     }
 
     _tick() {
-        
+        this.seconds += 1;
+        if (this.seconds === 60) {
+            this.seconds = 0;
+            this.minutes += 1;
+            if (this.minutes === 60) {
+                this.minutes = 0;
+                this.hours = (this.hours + 1) % 24;
+            }
+        }
+        // console.log(this);
+        this.printTime();
         
         // 1. Increment the time by one second.
         // 2. Call printTime.
@@ -32,7 +42,7 @@ class Clock {
 
 const clock = new Clock();
 
-console.log("Hi");
+// console.log(clock.printTime());
 
-console.log(new Date());
+
 
