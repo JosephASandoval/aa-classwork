ec30ccfb61a487d72f5669fab170946eddf9291d
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-01-19 15:02:50 -0800

    Finished

--- W9D2/prototypal_inheritance_exercises/prototypical_inheritance_exercises.js
+++ W9D2/prototypal_inheritance_exercises/prototypical_inheritance_exercises.js
@@ -1,20 +1,27 @@
-Function.prototype.inherits = function(superClass) {
-  function Surrogate() {};
-  Surrogate.prototype = superClass.prototype;
-  this.prototype = new Surrogate();
+// Function.prototype.inherits = function(superClass) {
+//   function Surrogate() {};
+//   Surrogate.prototype = superClass.prototype;
+//   this.prototype = new Surrogate();
+//   this.prototype.constructor = this;
+// }
+
+Function.prototype.inherits = function(superClass){
+  this.prototype = Object.create(superClass.prototype);
   this.prototype.constructor = this;
 }
 
+
 function MovingObject(title) {
     this.title = title;
-  }
+}
 
 MovingObject.prototype.makeMove = function () {
-  console.log(`${this.title} is moving`);
+  console.log(`${this.title} is moving, ${this.size}`);
 }
 
 function Ship(title, size) {
-  MovingObject.call(this, title);
+  // MovingObject.call(this, title);
+  this.title = title;
   this.size = size
 }
 
@@ -23,5 +30,10 @@ Ship.inherits(MovingObject);
 let myShip = new Ship("my ship", 23);
 myShip.makeMove();
 
-function Asteroid() {}
+function Asteroid(title, size) {
+  this.title = title;
+  this.size = size;
+}
 Asteroid.inherits(MovingObject);
+let myAst = new Asteroid("my Ast", 70);
+myAst.makeMove();
