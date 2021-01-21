ed89dea7da3c813259f6f8ecd55d85959b1ccb35
JosephASandoval <josephasandoval17@gmail.com>
2021-01-21 13:39:53 -0800

    Updated

--- W9D4/skeleton/app/assets/javascripts/bundle.js
+++ W9D4/skeleton/app/assets/javascripts/bundle.js
@@ -1,7 +1,82 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./frontend/follow_toggle.js":
+/*!***********************************!*\
+  !*** ./frontend/follow_toggle.js ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const { ids } = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module 'webpack'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()))
+
+class FollowToggle {
+  constructor(el) {
+    this.$el = $(el);
+    this.userId = $el.data("user-id");
+    this.followState = $el.data("initial-follow-state");
+    this.render();
+  }
+
+  render() {
+    if (this.followState === "unfollowed") {
+      return "Follow!"
+    } else {
+      return "Unfollow!"
+    }
+  }
+
+  handleClick() {
+
+    el.preventDefault();
+    
+
+  }
+}
+
+module.exports = FollowToggle;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+(() => {
 /*!*****************************!*\
   !*** ./frontend/twitter.js ***!
   \*****************************/
+const followToggle = __webpack_require__(/*! ./follow_toggle.js */ "./frontend/follow_toggle.js");
+
+$("button.follow-toggle").each((index, button_ele) => {
+  new followToggle(button_ele);
+})
+
+
+
+})();
 
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
--- W9D4/skeleton/frontend/follow_toggle.js
+++ W9D4/skeleton/frontend/follow_toggle.js
@@ -5,12 +5,22 @@ class FollowToggle {
     this.$el = $(el);
     this.userId = $el.data("user-id");
     this.followState = $el.data("initial-follow-state");
+    this.render();
   }
 
   render() {
     if (this.followState === "unfollowed") {
       return "Follow!"
+    } else {
+      return "Unfollow!"
     }
+  }
+
+  handleClick() {
+
+    el.preventDefault();
+    
+
   }
 }
 
