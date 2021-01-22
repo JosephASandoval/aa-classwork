60f3c457984866ef0ffa2112ca158e1061b8dbac
JosephASandoval <josephasandoval17@gmail.com>
2021-01-21 16:45:37 -0800

    Worked on users_search.js

--- W9D4/skeleton/app/assets/javascripts/bundle.js
+++ W9D4/skeleton/app/assets/javascripts/bundle.js
@@ -112,13 +112,9 @@ module.exports = FollowToggle;
 /*!**********************************!*\
   !*** ./frontend/users_search.js ***!
   \**********************************/
-/***/ ((module) => {
+/***/ (() => {
 
-class UsersSearch {
-  
-}
-
-module.exports = UsersSearch;
+throw new Error("Module parse failed: Unexpected token (6:2)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n|     this.$input =\n|     this.$ul =\n>   }\n| }\n| ");
 
 /***/ })
 
--- W9D4/skeleton/frontend/users_search.js
+++ W9D4/skeleton/frontend/users_search.js
@@ -1,5 +1,9 @@
 class UsersSearch {
-  
+  constructor(el) {
+    this.$el = $(el);
+    this.$input =
+    this.$ul =
+  }
 }
 
 module.exports = UsersSearch;
