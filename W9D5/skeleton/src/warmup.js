32571ecbd597e8d9c48a1cb757f2dfb1f8b35984
JosephASandoval <josephasandoval17@gmail.com>
2021-01-22 15:15:06 -0800

    Added W9D5

---
+++ W9D5/skeleton/src/warmup.js
@@ -0,0 +1,18 @@
+
+const partyHeader = document.getElementById('party'); 
+
+
+
+export const htmlGenerator = (string, htmlElement) => {
+  if (htmlElement.children) {
+    Array.from(htmlElement.children).forEach((child) =>
+      htmlElement.removeChild(child)
+    );
+  }
+  const pTag = document.createElement("p");
+  pTag.innerHTML = string;
+  htmlElement.appendChild(pTag);
+};
+
+htmlGenerator('Party Time.', partyHeader);
+htmlGenerator('Sleepy Time.', partyHeader);
