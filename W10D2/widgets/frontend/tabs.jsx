5aeb6311d1878919bfe6093bd6bc86f7bb0a6273
Qi Zhu <rubybfm1017@gmail.com>
2021-01-26 17:21:56 -0800

    start tabs

---
+++ W10D2/widgets/frontend/tabs.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Headers extends React.Component{
+    
+}
+
+class Tabs extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+        <h1>
+            Tabs
+        </h1>
+        )
+    }
+}
+
+export default Tabs;
--- W10D2/widgets/frontend/widgets.jsx
+++ W10D2/widgets/frontend/widgets.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Clock from './clock';
+import Tabs from './tabs'
 
 const Widgets = () => {
 
@@ -7,6 +8,7 @@ const Widgets = () => {
     <div>
       Congratulations, you did it!
       <Clock/>
+      <Tabs/>
     </div>
   )
 
