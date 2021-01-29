509cd49b6e273285556409e06166a6463d8ceb96
Andrew Curd <andrewc2695@gmail.com>
2021-01-29 13:34:02 -0800

    wrote steps action and reducer

--- W10D5/todos/frontend/todo_redux.jsx
+++ W10D5/todos/frontend/todo_redux.jsx
@@ -2,12 +2,17 @@ import React from "react";
 // import ReactDOM from "react-dom";
 import configureStore from "./store/store.js"
 import { receiveTodos, receiveTodo, removeTodo} from "./actions/todo_actions.js"
+import { receiveSteps, receiveStep, removeStep } from "./actions/step_actions.js"
+
 
 
 document.addEventListener ("DOMContentLoaded", () => {
   console.log("todo_redux");
   const root = document.getElementById("container")
   // ReactDOM.render (<Root />, root)
+  window.receiveStep = receiveStep;
+  window.receiveSteps = receiveSteps;
+  window.removeStep = removeStep;
 
   window.receiveTodo = receiveTodo;
   window.receiveTodos = receiveTodos;
