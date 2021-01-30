907b41c00df3db66eb075fb55c34c33fa65e8049
JosephASandoval <josephasandoval17@gmail.com>
2021-01-29 17:01:37 -0800

    Updated

--- W10D5/todos/frontend/components/todos/todo_form.jsx
+++ W10D5/todos/frontend/components/todos/todo_form.jsx
@@ -4,14 +4,43 @@ import uniqueId from "../uniqueId"
 class TodoForm extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            id: uniqueId(),
+            body: "",
+            title: ""
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
+    handleSubmit(e) {
+        e.prevenetDefault();
+
+        this.props.receiveTodo(this.state)
+        this.setState ({
+          id: uniqueId(),
+          body: "",
+          title: "",
+        });
+    }
 
     render(){
-        return(
-            
+        return (
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Title
+                <input type="text" value={this.state.title} />
+              </label>
+              <label>
+                Body
+                <input type="text" value={this.state.body} />
+              </label>
+              <button type="submit">Add New Todo</button>
+            </form>
+          </div>
         );
     }
 }
 
 export default TodoForm;
+
