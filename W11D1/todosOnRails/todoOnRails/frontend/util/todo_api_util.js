7ed629161528f45bc7c7f83aa3a3489c5db232a1
Andrew Curd <andrewc2695@gmail.com>
2021-02-01 15:39:31 -0800

    combined react and redux work from yesterday

---
+++ W11D1/todosOnRails/todoOnRails/frontend/util/todo_api_util.js
@@ -0,0 +1,11 @@
+import {$CombinedState } from "redux"
+
+// export const fetachAllTodos = () => {
+//     return $.ajax({
+//         method: 'GET',
+//         url: '/api/todos'
+//     }).then(
+//         todos => console.log(todos),
+//         error => console.log(error)
+//     );
+// }
