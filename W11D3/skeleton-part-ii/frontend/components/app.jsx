f6f11a197c2157ef67e233f0f65824c3e01eb83a
Chubbibanana <dongsoo.tchah@gmail.com>
2021-02-03 14:16:16 -0800

    Refactored pokemonindexitem

--- W11D3/skeleton-part-ii/frontend/components/app.jsx
+++ W11D3/skeleton-part-ii/frontend/components/app.jsx
@@ -1,5 +1,6 @@
 import { Route } from "react-router-dom";
 import React from "react";
+import PokemonIndexContainer from './../components/pokemon/pokemon_index_container';
 
  const App = () => (
    <Route path="/" component={PokemonIndexContainer} /> 
--- W11D3/skeleton-part-ii/frontend/components/root.jsx
+++ W11D3/skeleton-part-ii/frontend/components/root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
+import App from './app';
 import PokemonIndexContainer from './../components/pokemon/pokemon_index_container';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
+      {/* <Route path="/" component={PokemonIndexContainer} /> */}
       <App />
     </HashRouter>
   </Provider>
