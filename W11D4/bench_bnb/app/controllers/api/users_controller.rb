3043ea057fc521604467cdcfb96e7076cd2a3a2b
Janette <tinoconjanette@gmail.com>
2021-02-04 15:27:35 -0800

    Work on Users Controller

--- W11D4/bench_bnb/app/controllers/api/users_controller.rb
+++ W11D4/bench_bnb/app/controllers/api/users_controller.rb
@@ -1,3 +1,9 @@
 class Api::UsersController < ApplicationController
-  
+  def create
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password_digest, :password, :session_token)
+  end
 end
--- W11D4/bench_bnb/app/models/user.rb
+++ W11D4/bench_bnb/app/models/user.rb
@@ -4,29 +4,38 @@ class User < ApplicationRecord
   validates :password_digest, presence: true
   validates :session_token, presence: true, uniqueness: true
 
+  after_inititalize :ensure_session_token
 
+  attr_reader :password
 
   def self.find_by_credentails(username, password)
-
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      return user
+    else
+      nil
+    end
   end
 
-  def password=()
+  def password=(password)
     @password = password
-    
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_password?(password)
-
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   def reset_session_token!
-
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
   end
 
   private
 
   def ensure_session_token
-
+    self.session_token ||= SecureRandom::urlsafe_base64
   end
 
 end
