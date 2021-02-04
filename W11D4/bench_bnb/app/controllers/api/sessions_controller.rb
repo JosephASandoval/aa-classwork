e448551af44f68d4d04b8da0faa6e6e50399af54
JosephASandoval <josephasandoval17@gmail.com>
2021-02-04 15:40:50 -0800

    Updated

--- W11D4/bench_bnb/app/controllers/api/sessions_controller.rb
+++ W11D4/bench_bnb/app/controllers/api/sessions_controller.rb
@@ -1,3 +1,13 @@
 class Api::SessionsController < ApplicationController
-    
+  def create
+    @user = User.find_by_credentails(
+      params[:user][:username],
+      params[:user][:password]
+    )
+  end
+
+  def destroy
+
+  end
+  
 end
--- W11D4/bench_bnb/app/controllers/api/users_controller.rb
+++ W11D4/bench_bnb/app/controllers/api/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      render json: @user.errors.full_messages, status: 401
+      # might have to add a new
+    end
   end
 
   private
