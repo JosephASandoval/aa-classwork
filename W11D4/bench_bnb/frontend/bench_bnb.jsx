3fe2a0a70e9df7f8851f936abc5acd1114b7a1e9
JosephASandoval <josephasandoval17@gmail.com>
2021-02-05 09:47:19 -0800

    Updated

--- W11D4/bench_bnb/frontend/bench_bnb.jsx
+++ W11D4/bench_bnb/frontend/bench_bnb.jsx
@@ -2,8 +2,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { login, signup, logout } from "./util/session_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
+
 });
