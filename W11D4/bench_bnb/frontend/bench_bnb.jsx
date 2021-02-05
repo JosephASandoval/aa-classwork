7b81dd7d0a94dfe9b185a9c969fe745f91c281db
Janette <tinoconjanette@gmail.com>
2021-02-05 12:21:14 -0800

    Start Containers in BNB

--- W11D4/bench_bnb/frontend/bench_bnb.jsx
+++ W11D4/bench_bnb/frontend/bench_bnb.jsx
@@ -2,18 +2,29 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import Root from './components/root';
+import configureStore from './store/store'
+
 import { login, signup, logout } from "./util/session_api_util"
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 
   
 
   window.login = login;
   window.logout = logout;
   window.signup = signup;
-  window.store = store
+
+  const store = configureStore();
+  window.store = store;
+  // we don't put the store directly on the window because
+  // it can be confusing when debugging, sometimes giving you access to state
+  // when you shouldn't
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; // just for testing!
 
 });
--- W11D4/bench_bnb/frontend/reducers/users_reducer.js
+++ W11D4/bench_bnb/frontend/reducers/users_reducer.js
@@ -6,6 +6,6 @@ export const usersReducer = (state = {}, action) => {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, state, { [action.user.id]: action.user })
         default:
-            state;
+            return state;
     }
 }
