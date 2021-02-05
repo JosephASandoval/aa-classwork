de7f198adc28c8e56acca396bcc51bdb4527f6ac
JosephASandoval <josephasandoval17@gmail.com>
2021-02-05 11:59:53 -0800

    Updated

--- W11D4/bench_bnb/frontend/reducers/session_errors_reducer.js
+++ W11D4/bench_bnb/frontend/reducers/session_errors_reducer.js
@@ -4,7 +4,7 @@ export const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return state;
+            return [];
         case RECEIVE_SESSION_ERRORS:
             return action.errors;
         default:
