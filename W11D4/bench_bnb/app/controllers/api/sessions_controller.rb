ddfcf58a029abb9d5fc4758f0dc1e5a5b7ca6e17
JosephASandoval <josephasandoval17@gmail.com>
2021-02-04 16:24:54 -0800

    Updated

--- W11D4/bench_bnb/app/controllers/api/sessions_controller.rb
+++ W11D4/bench_bnb/app/controllers/api/sessions_controller.rb
@@ -1,13 +1,23 @@
 class Api::SessionsController < ApplicationController
+
   def create
     @user = User.find_by_credentails(
       params[:user][:username],
       params[:user][:password]
     )
+
+    if @user
+      login!(@user)
+      # come back and fix this
+    else
+      render json: ["Invalid user credentails"], status 401
+    end
+
   end
 
   def destroy
-
+    logout!
+    redirect_to new_session_url # double check later
   end
   
 end
