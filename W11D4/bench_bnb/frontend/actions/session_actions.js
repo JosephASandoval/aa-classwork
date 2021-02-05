ccda4a74a6bd8598c3dba4809cece9eabf5113f2
Janette <tinoconjanette@gmail.com>
2021-02-05 11:30:44 -0800

    Start errors Reducer in BNB

--- W11D4/bench_bnb/frontend/actions/session_actions.js
+++ W11D4/bench_bnb/frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as APIUtil from '../util/session_api_util'
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER'
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS'
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_ERRORS'
 
 //plain action creators
 export const receiveCurrentUser = (currentUser) => {
@@ -20,7 +20,7 @@ export const logoutCurrentUser = () => {
 
 export const receiveErrors = (errors) => {
     return ({
-        type: RECEIVE_ERRORS,
+        type: RECEIVE_SESSION_ERRORS,
         errors
     })
 }
